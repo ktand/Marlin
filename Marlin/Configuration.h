2e764666944e797ebc4335385d8f3d85603387e2
Karl Andersson <karl@iaccess.se>
2019-01-16 20:47:39 +0100

    Bondtech extruder e-steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -651,7 +651,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 156.84 } // 16/16/32/32 uSteps
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 412.61 } // 16/16/32/32 uSteps
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -825,7 +825,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 24  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 9   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.95   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.73   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 0
